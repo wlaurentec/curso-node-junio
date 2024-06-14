840d2abd9b17d0487350864070c187c84efaf89b
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-14 16:51:45 -0500

    handling http modules

--- app.js
+++ app.js
@@ -1,42 +1,12 @@
-function ordenarProducto(producto) {
-  return new Promise((resolve, reject) => {
-    console.log(`1.0: Ordenando: ${producto} de freeCodeCamp`);
-    setTimeout(() => {
-      if (producto === "taza") {
-        resolve("Pedido tomado: taza de freeCodeCamp...");
-      } else {
-        reject("Lo siento, no tenemos este producto");
-      }
-    }, 2000);
-  });
-}
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log("request made to server");
+  console.log(req.url);
+  console.log(req.method);
+  res.end("Hello World");
+});
 
-function procesarPedido(respuesta) {
-  return new Promise((resolve, _reject) => {
-    console.log(`2.0: Procesando respuesta...`);
-    console.log(`3.0:La respuesta fue: ${respuesta}`);
+const port = 3000;
 
-    setTimeout(() => {
-      resolve("Gracias por tu compra. Disfruta tu pedido");
-    }, 4000);
-  });
-}
+server.listen(port,() => console.log(`Server has started on port http://localhost:${port}`));
 
-/* ordenarProducto("taza")
-  .then(procesarPedido)
-  .then((mensaje) => console.log(mensaje))
-  .catch((error) => console.log(error)); */
-
-
-async function realizarPedido(producto) {
-  try {
-    const respuesta = await ordenarProducto(producto);
-    console.log(respuesta);
-    const procesamiento = await procesarPedido(respuesta);
-    console.log(procesamiento);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-realizarPedido("tazas");
