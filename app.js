e7c9ced77058265e43209136c95d91fb271bfbc3
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-14 18:13:04 -0500

    handling routing in node.js

--- app.js
+++ app.js
@@ -1,6 +1,74 @@
-const miURL = new URL("https://www.youtube.com/watch?v=1hpc70_OoAg&t=15059s");
+const http = require("http");
+const cursos = require("./cursos.js");
+const { futimesSync } = require("fs");
+const server = http.createServer((req, res) => {
+  const { method } = req;
 
-console.log(miURL.hostname);
-console.log(miURL.pathname);
-console.log(miURL.searchParams);
-console.log(miURL.toString());
+  switch (method) {
+    case "GET":
+      return manejarGET(req, res);
+    case "POST":
+      return manejarPOST(req, res);
+    case "PUT":
+      return manejarPUT(req, res);
+    case "DELETE":
+      return manejarDELETE(req, res);
+    default:
+      console.log(`Método ${method} no soportado`);
+  }
+});
+
+function manejarGET(req, res) {
+  const path = req.url;
+  if (path === "/") {
+    res.statusCode = 200;
+    return res.end("Bienvenido a mi primer servidor y API creados con Node.js");
+  } else if (path === "/cursos") {
+    res.statusCode = 200;
+    return res.end(JSON.stringify(cursos));
+  } else if (path === "/cursos/matematicas") {
+    res.statusCode = 200;
+    return res.end(JSON.stringify(cursos.infoCursos.matematicas));
+  } else if (path === "/cursos/programacion") {
+    res.statusCode = 200;
+    return res.end(JSON.stringify(cursos.infoCursos.programacion));
+  } else {
+    res.statusCode = 404;
+    return res.end("No se encontro el recurso solicitado");
+  }
+}
+
+function manejarPOST(req, res) {
+  const path = req.url;
+  if (path === "/cursos/programacion") {
+    res.statusCode = 201;
+    res.end("Se ha creado un nuevo recurso");
+  } else {
+    res.statusCode = 404;
+    res.end("No se encontro el recurso solicitado");
+  }
+}
+
+function manejarPUT(req, res) {
+  const path = req.url;
+  if (path === "/cursos/programacion") {
+    res.statusCode = 200;
+    res.end("Se ha actualizado el recurso");
+  } else {
+    res.statusCode = 404;
+    res.end("No se encontro el recurso solicitado");
+  }
+}
+
+function manejarDELETE(req, res) {
+  const path = req.url;
+  if (path === "/cursos/programacion") {
+    res.statusCode = 200;
+    res.end("Se ha eliminado el recurso");
+  } else {
+    res.statusCode = 404;
+    res.end("No se encontro el recurso solicitado");
+  }
+}
+
+server.listen(3000, () => console.log("server running on port 3000..."));
---
+++ cursos.js
@@ -0,0 +1,20 @@
+let infoCursos = {
+  programacion: [
+    {
+      nombre: "Responsive Web Design",
+      precio: 200,
+    },
+    {
+      nombre: "JavaScript Moderno",
+      precio: 300,
+    },
+  ],
+  matematicas: [
+    {
+      nombre: "Calculo I",
+      precio: 100,
+    },
+  ],
+}
+
+module.exports.infoCursos = infoCursos
